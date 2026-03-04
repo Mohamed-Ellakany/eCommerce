5a4d1644d256315fda70efae07ad0fc0b3a1fd75
Rawda Ashor <141156812+rawda2@users.noreply.github.com>
2026-03-04 16:06:02 +0200

    Handle all Navigations + handle signUp & profile Toggle (#20)
    Co-authored-by: rawda ashour <ashrawda@gmain.com>

---
+++ Scripts/script.js
@@ -0,0 +1,23 @@
+function logout() {
+  localStorage.removeItem("shop_session");
+  window.location.href = "login.html";
+  console.log(this);
+}
+
+
+
+window.onload=function () {
+    let userData = localStorage.getItem("shop_session");
+    if (userData) {
+      userData = JSON.parse(userData);
+      const profileLink = document.getElementById("profileLink");
+      profileLink.textContent = "Profile";
+      profileLink.href = "Profile.html";
+      const signUpLink = document.getElementById("signUp");
+      signUpLink.textContent = "";
+      const logoutBtn = document.getElementById("logout");
+      logoutBtn.classList.remove("d-none");
+      console.log(logout);
+      logoutBtn.addEventListener("click", logout);
+    }
+}
