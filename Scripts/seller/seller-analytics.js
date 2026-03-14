e6768fe0e55fb31f84fcd27cb495bab056e6eacf
Mohamed Ellakany <136765196+Mohamed-Ellakany@users.noreply.github.com>
2026-03-14 21:41:34 +0200

    Remove some un used code (#71)
    * removeInitailUserFromUi
    
    * add style link
    
    * fix product catalog
    
    * edit url of data

--- Scripts/seller/seller-analytics.js
+++ Scripts/seller/seller-analytics.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://e-commerce-server-xi.vercel.app";
+const BASE_URL = "https://json-server-for-ecomerce-app-cst.vercel.app";
 
 (async () => {
   const session = DB.getSession();
